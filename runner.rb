fd12ee676bb18e8149746bb5ecc5d3790ec58e94
Emily Port <emilylport@gmail.com>
2022-09-23 14:19:45 -0600

    Add .length to the puts statement

--- runner.rb
+++ runner.rb
@@ -8,4 +8,4 @@ require './lib/night_writer'
 content = File.read(ARGV[0])
 night_writer = NightWriter.new(ARGV[0], ARGV[1])
 File.write(ARGV[1], content)
-puts " Created #{ARGV[1]} containing #{ARGV[1]} characters"
+puts " Created #{ARGV[1]} containing #{ARGV[1].length} characters"
