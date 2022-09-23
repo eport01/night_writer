6336a6c0ae22c12b352619a61ec955f06247cd0d
Emily Port <emilylport@gmail.com>
2022-09-23 15:07:56 -0600

    dictionary can translate one letter

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,7 +1,11 @@
 class Dictionary
   attr_reader :alphabet
   def initialize
-    @alphabet = [{"h" => "0.\n00\n.."}]
+    @alphabet = [{"a" => "0.\n..\n.."},{"h" => "0.\n00\n.."}]
+  end
+
+  def translate_letter(letter)
+    @alphabet.select {|letters| letters.find {|eng, braille| eng  == letter}}[0].values[0]
   end
 
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,11 +4,13 @@ require './lib/dictionary'
 
 RSpec.describe Dictionary do
   it 'exists' do 
-
+    dictionary = Dictionary.new
+    expect(dictionary).to be_an_instance_of(Dictionary)
   end
 
   it 'changes a to the braille equivalent' do 
     dictionary = Dictionary.new
+    # require 'pry'; binding.pry
     expect(dictionary.translate_letter("h")).to eq( "0.\n00\n..")
   end
 end
