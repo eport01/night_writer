e5acc471b1ca4a2790aaca1cee3dfb6fd135adeb
Emily Port <emilylport@gmail.com>
2022-09-25 17:11:13 -0600

    Fix spacing in dictionary class and spec files

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,7 +2,6 @@
 class Dictionary
   attr_reader :alphabet, :translate_letter, :translate_braille
   def initialize
-    # @night_writer = NightWriter.new(@input_file, @output_file)
     @alphabet = [
       {"a" => "0.\n..\n.."},
       {"b" => "0.\n0.\n.."},
@@ -34,11 +33,7 @@ class Dictionary
     ]
   end
 
-  #write a method that takes a word in and splits it into individual letters,
-  #call method on each letter 
-
   def translate_letter(letter)
-    # @alphabet.select {|letters| letters.find {|eng, braille| p braille if eng  == letter}}[0].values.flatten
     test = @alphabet.select {|letters| letters.find {|eng, braille| eng  == letter}}[0].values[0]
     
   end
@@ -47,17 +42,5 @@ class Dictionary
     @alphabet.select {|letters| letters.find {|eng, braille| braille  == character}}[0].keys[0]
 
   end
-
-  #maybe move alphabet to method 
-
-  # def translate 
-  #   @input_file
-  #   require 'pry'; binding.pry
-  # end
-
 end
-# dictionary = Dictionary.new 
-# p dictionary.translate
-# [h = "0.\n00\n.."]
-# puts h 
-# dictionary = Dictionary.new
+
