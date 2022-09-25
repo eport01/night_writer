7cd380b47c4eb9bcc8a205b11b1acb4c45dff1b6
Emily Port <emilylport@gmail.com>
2022-09-25 16:33:21 -0600

    Add fixture files to night_reader spec and update file paths

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -3,18 +3,16 @@ require './lib/night_reader'
 require './lib/dictionary'
 
 RSpec.describe NightReader do 
-  # before(:each) do 
-  #   @night_reader = NightReader.new('./reader_input.txt', './reader_output.txt')
-  # end
+
   it 'exists' do 
-    night_reader = NightReader.new('./reader_input.txt', './reader_output.txt')
+    night_reader = NightReader.new('./fixture_files/reader_input.txt', './fixture_files/reader_output.txt')
     expect(night_reader).to be_an_instance_of(NightReader)
   end
 
   it 'can read an input file' do 
-    night_reader = NightReader.new('./reader_input.txt', './reader_output.txt')
-    allow(night_reader).to receive(:input_file).and_return('./reader_input.txt')
-    expect(night_reader.night_read_file).to eq("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n\n")
-# require 'pry'; binding.pry
+    night_reader = NightReader.new('./fixture_files/reader_input.txt', './fixture_files/reader_output.txt')
+    allow(night_reader).to receive(:input_file).and_return('./fixture_files/reader_input.txt')
+    expected = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0..."
+    expect(night_reader.night_read_file).to eq(expected)
   end
 end
