ad84100a65e926089520a8814595e41577c9584b
Emily Port <emilylport@gmail.com>
2022-09-25 16:23:42 -0600

    Move fixture files into their own folder, updated night writer spec with file paths

--- runner.rb
+++ runner.rb
@@ -9,14 +9,12 @@ require './lib/night_reader'
 # ARGV[0] = 'message.txt'
 # ARGV[1] = 'braille.txt'
 
-# night_writer = NightWriter.new(ARGV[0], ARGV[1])
-# night_writer.braille_print
+night_writer = NightWriter.new(ARGV[0], ARGV[1])
+night_writer.braille_print
 
-night_reader = NightReader.new(ARGV[0], ARGV[1])
-# require 'pry'; binding.pry
-night_reader.english_print
-# night_reader.split_lines
-# require 'pry'; binding.pry
+# night_reader = NightReader.new(ARGV[0], ARGV[1])
+# night_reader.english_print
+
 
 
 
